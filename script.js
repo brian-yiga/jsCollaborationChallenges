d25de54fcb7e740c13b657feafb024b2be97d788
Jacobokia <cobs399@gmail.com>
2025-03-20 22:52:10 +0300

    add challenges and solutions

--- script.js
+++ script.js
@@ -145,3 +145,63 @@ let y = 7;
 
 [x, y] = [y, x];
 console.log(x, y);
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly** one solution**, and you may not use the same element twice.
+You can return the answer in any order.
+‌
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+*/
+
+//Use a function with nums and target as parameters
+//Loop through the array to pick the position of the indices of the nums that add up to the target
+//Return the corresponding indices of the two nums
+
+const nums = [11, 15, 2, 7];
+const target = 9;
+
+const checkNums = function (nums, target) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      sum = nums[i] + nums[j];
+      if (sum === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(checkNums(nums, target));
+
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+
+
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+
+
+*/
+
+const makeNegative = function (number) {
+  if (number > 0) {
+    return -number;
+  } else if (number < 0) {
+    return number;
+  } else {
+    return 0;
+  }
+};
+
+console.log(makeNegative(1));
+console.log(makeNegative(-5));
+console.log(makeNegative(0));
