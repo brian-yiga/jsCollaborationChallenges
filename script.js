98b75fc99e592a51cf31a26d795bfe11b244d83d
brianyiga <64937222+brian-yiga@users.noreply.github.com>
2025-03-20 13:33:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/*Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1**2+2**2+2**2=9.
+
+*/
+//How to square numbers num**num
+// //todo loop through the array using a for loop
+// function squareSum(numbers){
+//     let sum = 0;
+//     for (let i = 0; i<numbers.length; i++) {
+//         const number = numbers[i]*numbers[i]
+//         // console.log(number);   
+//         sum += number;
+//     }
+//     return sum;
+// }
+
+// console.log(squareSum([1, 2, 2]))
+
+//todo loop through the array using a for..of loop
+
+// const numbers = [1, 2, 2];
+// function squareSum(numbers) {
+//     let sum = 0;
+//     for (let num of numbers) {
+//         sum += num**2;
+//     }
+//     return sum;
+    
+// }
+
+// const result = squareSum(numbers);
+// console.log(result);
+
