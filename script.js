63b4f5efe4ba19b5e408035cf3393e16295ffd7c
brianyiga <64937222+brian-yiga@users.noreply.github.com>
2025-03-24 13:03:52 +0300

    Add solutions to destructuring exercises

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ firstColor should be "red"
 
 secondColor should be "green"
 */
-const colors = ["red", "green", "blue"];
-const [firstColor, secondColor] = colors;
-console.log(firstColor, secondColor);
+// const colors = ["red", "green", "blue"];
+// const [firstColor, secondColor] = colors;
+// console.log(firstColor, secondColor);
 
 /*
 Given the following object
@@ -65,13 +65,13 @@ age should be 28
 city should be "Unknown"
 */
 
-const user = {
-  name: "Alice",
-  age: 28,
-};
+// const user = {
+//   name: "Alice",
+//   age: 28,
+// };
 
-const { name, age, city = "Unknown" } = user;
-console.log(name, age, city);
+// const { name, age, city = "Unknown" } = user;
+// console.log(name, age, city);
 
 /*
 Given the following object;
@@ -86,16 +86,16 @@ name should be "Bob"
 
 position should be "Developer"
 */
-const employee = {
-  id: 1,
-  details: {
-    firstName: "Bob",
-    position: "Developer",
-  },
-};
+// const employee = {
+//   id: 1,
+//   details: {
+//     firstName: "Bob",
+//     position: "Developer",
+//   },
+// };
 
-const { firstName, position } = employee.details;
-console.log(firstName, position);
+// const { firstName, position } = employee.details;
+// console.log(firstName, position);
 
 /*
 Given the following array:
@@ -111,20 +111,20 @@ personName should be "Eve"
 personAge should be 30
 */
 
-const people = [
-  {
-    name: "Eve",
-    age: 30,
-  },
-  {
-    name: "John",
-    age: 25,
-  },
-];
+// const people = [
+//   {
+//     name: "Eve",
+//     age: 30,
+//   },
+//   {
+//     name: "John",
+//     age: 25,
+//   },
+// ];
 
-const [{ name: personName, age: personAge }] = people;
+// const [{ name: personName, age: personAge }] = people;
 
-console.log(personName, personAge);
+// console.log(personName, personAge);
 
 /*
 Given two variables:
@@ -140,11 +140,11 @@ x should be 7
 y should be 3
 */
 
-let x = 3;
-let y = 7;
+// let x = 3;
+// let y = 7;
 
-[x, y] = [y, x];
-console.log(x, y);
+// [x, y] = [y, x];
+// console.log(x, y);
 
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -162,22 +162,22 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
 //Loop through the array to pick the position of the indices of the nums that add up to the target
 //Return the corresponding indices of the two nums
 
-const nums = [11, 15, 2, 7];
-const target = 9;
+// const nums = [11, 15, 2, 7];
+// const target = 9;
 
-const checkNums = function (nums, target) {
-  let sum = 0;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
-      sum = nums[i] + nums[j];
-      if (sum === target) {
-        return [i, j];
-      }
-    }
-  }
-};
+// const checkNums = function (nums, target) {
+//   let sum = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = 1; j < nums.length; j++) {
+//       sum = nums[i] + nums[j];
+//       if (sum === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
 
-console.log(checkNums(nums, target));
+// console.log(checkNums(nums, target));
 
 /*
 In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
@@ -192,16 +192,124 @@ makeNegative(0);    // return 0
 
 */
 
-const makeNegative = function (number) {
-  if (number > 0) {
-    return -number;
-  } else if (number < 0) {
-    return number;
-  } else {
-    return 0;
-  }
+// const makeNegative = function (number) {
+//   if (number > 0) {
+//     return -number;
+//   } else if (number < 0) {
+//     return number;
+//   } else {
+//     return 0;
+//   }
+// };
+
+// console.log(makeNegative(1));
+// console.log(makeNegative(-5));
+// console.log(makeNegative(0));
+
+
+
+/*Destructuring Nested Objects - Flatten Object
+You are given an object with a nested object representing the address of a person. Destructure the object and flatten the address properties (e.g., street, city, zip) into the main object.
+
+Example Input:
+const user = { 
+    name: "Eve", 
+    address: { 
+            street: "123 Main St", 
+            city: "Springfield", 
+            zip: "12345",
+        }, 
+    };
+
+
+Example Output:
+
+name: "Eve", street: "123 Main St", city: "Springfield", zip: "12345"*/
+
+// const user = { 
+//     name: "Eve", 
+//     address: { 
+//             street: "123 Main St", 
+//             city: "Springfield", 
+//             zip: "12345",
+//         }, 
+//     };
+
+//DESTRUCTURE ADDRESS FROM USER 
+// const { street, city, zip } = user.address;
+// console.log(street, city, zip)
+
+// const {name} = user;
+// console.log(name);
+
+// console.log(`name: ${name}, street: ${street}, city: ${city}, zip: ${zip}`);
+
+// const { name, address: { street, city, zip },} = user;
+// console.log(name, street, city, zip);
+
+//refactored code
+// const { name, address: { street, city, zip } } = user;
+
+// console.log(`name: ${name}, street: ${street}, city: ${city}, zip: ${zip}`);
+
+
+
+/*Destructuring Arrays - Skipping Elements
+You are given an array of numbers. Use array destructuring to extract the first two elements and skip the third one. Put the remaining elements into a new array rest
+
+Example Input:
+
+const numbers = [1, 2, 3, 4, 5];
+
+Example Output:
+
+first: 1, second: 2, rest: [4, 5]*/
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const [first, second, , ...rest] = numbers;
+// console.log(`first: ${first}, second: ${second}, rest: [${rest}]`);
+
+
+/*Destructuring in Function Parameters
+
+Write a function that takes an object as a parameter and destructures it to extract name, age, and country. Provide default values for age (25) and country ("USA") if they are not provided.
+
+Example Input:
+
+function greetUser({ name, age = 25, country = "USA" }) { return Hello, ${name}. You are ${age} years old and from ${country}.; } const user = { name: "Charlie" };
+
+Expected Output:
+
+"Hello, Charlie. You are 25 years old and from USA."*/
+
+// function greetUser({ name, age = 25, country = "USA" }) { 
+//     return `Hello, ${name}. You are ${age} years old and from ${country}`;
+
+// } 
+
+
+//     const user = { name: "Charlie" };
+
+//     const result = greetUser(user);
+//     console.log(result);
+
+// console.log(user);
+
+
+/*Destructuring with Functions - Extract and Return Specific Values
+
+You are given a function getUserInfo that receives an object as an argument. The object contains properties name, age, city, and country. Use destructuring to extract name and city, and return a string with this information. If any of these properties are missing, use default values: "Unknown" for name and "Unknown City" for city.*/
+
+const getUserInfo = function ({name = 'Unknown', city = 'Unknown City', age, country}) {
+    return `Hello, ${name}. You are ${age} years old, you come from ${city} in ${country}`;
+}
+
+const user = {
+    // name: 'Brian',
+    age: 30,
+    // city: 'Kampala',
+    country: 'Uganda'
 };
 
-console.log(makeNegative(1));
-console.log(makeNegative(-5));
-console.log(makeNegative(0));
+console.log(getUserInfo(user));
